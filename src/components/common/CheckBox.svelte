9ab1aa592e73cae23dc9bebff69d4028eddcf1af
Anthony Hofmeister <anthonyhofmeister@gmx.de>
2023-01-16 21:11:48 +0100

    CheckBox Component

---
+++ src/components/common/CheckBox.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+    import { Checkmark } from "carbon-icons-svelte";
+
+    export let toggled: boolean
+    export let text: string = ""
+</script>
+
+<div class="checkbox-body">
+    <div class="checkbox" on:click={() => toggled = !toggled}>
+        <div class="inner-checkbox" class:toggled={toggled}>
+            <Checkmark size={16}/>
+        </div>
+    </div>
+
+    {#if text !== ""}
+        <span>Toggle</span>
+    {/if}
+</div>
+
+<style lang="scss">
+    .checkbox-body {
+        display: flex;
+        align-items: center;
+
+        user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        -webkit-user-select: none;
+
+        .checkbox {
+            width: 28px;
+            height: 28px;
+            background: white;
+            border: 1px solid gray;
+            border-radius: 6px;
+
+            cursor: pointer;
+
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            .inner-checkbox {
+                width: 28px;
+                height: 28px;
+
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                scale: 0.2;
+                opacity: 0;
+
+                transition: scale 0.2s ease, opacity 0.2s ease;
+
+                &.toggled {
+                    scale: 1;
+                    opacity: 1;
+                }
+            }
+        }
+
+        span {
+            margin-left: 0.6rem;
+            font-family: Inter, sans-serif;
+        }
+    }
+</style>
+
